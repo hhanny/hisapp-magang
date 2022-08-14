ff10adae56e49c62ad14291a55ff413ed1e98e3b
unknown <putrisetiyaningrum99@gmail.com>
2022-08-14 23:05:23 +0700

    push

---
+++ modules/registrasi/Module.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\modules\registrasi;
+
+/**
+ * registrasi module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'app\modules\registrasi\controllers';
+    public $id = 'Registrasi';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
