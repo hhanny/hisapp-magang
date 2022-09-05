22656baa0668f76658586d10a88edf96138b3942
hhanny <hannybeeee@gmail.com>
2022-09-05 09:18:53 +0700

    menambahkan dropdownlist (blm bisa terpanggil data pada database)

--- modules/medical/views/mrrekammedik/_form.php
+++ modules/medical/views/mrrekammedik/_form.php
@@ -16,24 +16,95 @@ use app\models\Mrrekammedik;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'jenisctev_id')->textInput(['maxlength' => true]) ?>
-    <?= $form->field($model, 'jenisctev_id')->dropDownList(ArrayHelper::map(Mrrekammedik::getActive(), 'supplier_id', 'name'), ['prompt' => 'Select', 'class' => 'select2-show-search form-select select2'])->label(false) ?>
+    <?php
+    $dataPost=ArrayHelper::map(\app\models\Mrjenisctev::find()
+        ->asArray()->all(), 'jenisctev_id', 'Jenisctev ID');
+    echo $form->field($model, 'jenisctev_id')
+        ->dropDownList(
+            $dataPost,
+            ['jenisctev_id'=>'Jenisctev ID']
+        );
+    ?>
+    
+    <?php
+    $dataPost=ArrayHelper::map(\app\models\Mrinfeksinosokomial::find()
+        ->asArray()->all(), 'infonoso_id', 'Infonoso ID');
+    echo $form->field($model, 'infonoso_id')
+        ->dropDownList(
+            $dataPost,
+            ['infonoso_id'=>'Infonoso ID']
+        );
+    ?>
 
-    <?= $form->field($model, 'infonoso_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrkasus::find()
+            ->asArray()->all(), 'kasus_id', 'Kasus ID');
+        echo $form->field($model, 'kasus_id')
+            ->dropDownList(
+                $dataPost,
+                ['kasus_id'=>'Kasus ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'kasus_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrstatuskembali::find()
+            ->asArray()->all(), 'statuskembali_id', 'Statuskembali ID');
+        echo $form->field($model, 'statuskembali_id')
+            ->dropDownList(
+                $dataPost,
+                ['statuskembali_id'=>'Statuskembali ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'statuskembali_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrdisabilitas::find()
+            ->asArray()->all(), 'tuna_kode', 'Tuna Kode');
+        echo $form->field($model, 'tuna_kode')
+            ->dropDownList(
+                $dataPost,
+                ['tuna_kode'=>'Tuna Kode']
+            );
+    ?>
 
-    <?= $form->field($model, 'tuna_kode')->textInput(['maxlength' => true]) ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrstatuslengkap::find()
+            ->asArray()->all(), 'statuslengkap_id', 'Statuslengkap ID');
+        echo $form->field($model, 'statuslengkap_id')
+            ->dropDownList(
+                $dataPost,
+                ['statuslengkap_id'=>'Statuslengkap ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'statuslengkap_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrugdlayanan::find()
+            ->asArray()->all(), 'ugdlayanan_id', 'Ugdlayanan ID');
+        echo $form->field($model, 'ugdlayanan_id')
+            ->dropDownList(
+                $dataPost,
+                ['ugdlayanan_id'=>'Ugdlayanan ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'ugdlayanan_id')->textInput(['maxlength' => true]) ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrugdalasandirujuk::find()
+            ->asArray()->all(), 'alasandirujuk_id', 'Alasandirujuk ID');
+        echo $form->field($model, 'alasandirujuk_id')
+            ->dropDownList(
+                $dataPost,
+                ['alasandirujuk_id'=>'Alasandirujuk ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'alasandirujuk_id')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'ugddiagnosa_id')->textInput(['maxlength' => true]) ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrdiagnosaawal::find()
+            ->asArray()->all(), 'ugddiagnosa_id', 'Ugddiagnosa ID');
+        echo $form->field($model, 'ugddiagnosa_id')
+            ->dropDownList(
+                $dataPost,
+                ['ugddiagnosa_id'=>'Ugddiagnosa ID']
+            );
+    ?>  
 
     <?= $form->field($model, 'no_reg')->textInput(['maxlength' => true]) ?>
 
--- modules/medical/views/mrrekammedik/create.php
+++ modules/medical/views/mrrekammedik/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Mrrekammedik */
 
-$this->title = 'Create Mrrekammedik';
+$this->title = 'Create New Medical Record';
 $this->params['breadcrumbs'][] = ['label' => 'Mrrekammediks', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
