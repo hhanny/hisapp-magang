4cb3d1cc619b4828a7db41df3903281b762c21ac
unknown <putrisetiyaningrum99@gmail.com>
2022-09-13 11:47:12 +0700

    menambah dropdown table mr

--- modules/medical/views/mrrekammedik/_form.php
+++ modules/medical/views/mrrekammedik/_form.php
@@ -16,28 +16,93 @@ use yii\helpers\ArrayHelper;
 
     <?php
     $dataPost=ArrayHelper::map(\app\models\Mrjenisctev::find()
-        ->asArray()->all(), 'jenisctev_id', 'Jenisctev ID');
+        ->asArray()->all(), 'jenisctev_id', 'jenis_ctev');
     echo $form->field($model, 'jenisctev_id')
         ->dropDownList(
-            ['01'=>'clubfoot','02'=>'jari keriting']
+            $dataPost,
+            // ['jenisctev_id'=>'Jenisctev ID']
         );
-    ?> 
+    ?>
 
-    <?= $form->field($model, 'infonoso_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrinfeksinosokomial::find()
+            ->asArray()->all(), 'infonoso_id', 'infeksi_nosokomial');
+        echo $form->field($model, 'infonoso_id')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+        ?>
+    
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrkasus::find()
+            ->asArray()->all(), 'kasus_id', 'nama_kasus',);
+        echo $form->field($model, 'kasus_id')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'kasus_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrstatuskembali::find()
+            ->asArray()->all(), 'statuskembali_id', 'nama_statuskembali',);
+        echo $form->field($model, 'statuskembali_id')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'statuskembali_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrdisabilitas::find()
+            ->asArray()->all(), 'tuna_kode', 'tuna_nama',);
+        echo $form->field($model, 'tuna_kode')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'tuna_kode')->textInput(['maxlength' => true]) ?>
- 
-    <?= $form->field($model, 'statuslengkap_id')->textInput() ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrstatuslengkap::find()
+            ->asArray()->all(), 'statuslengkap_id', 'nama_statuslengkap',);
+        echo $form->field($model, 'tuna_kode')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'ugdlayanan_id')->textInput(['maxlength' => true]) ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrugdlayanan::find()
+            ->asArray()->all(), 'ugdlayanan_id', 'ugd_layanan',);
+        echo $form->field($model, 'ugdlayanan_id')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'alasandirujuk_id')->textInput(['maxlength' => true]) ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrugdalasandirujuk::find()
+            ->asArray()->all(), 'alasandirujuk_id', 'alasan_dirujuk');
+        echo $form->field($model, 'alasandirujuk_id')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
-    <?= $form->field($model, 'ugddiagnosa_id')->textInput(['maxlength' => true]) ?>
+    <?php
+        $dataPost=ArrayHelper::map(\app\models\Mrdiagnosaawal::find()
+            ->asArray()->all(), 'ugddiagnosa_id', 'keterangan',);
+        echo $form->field($model, 'ugddiagnosa_id')
+            ->dropDownList(
+                $dataPost,
+                // ['jenisctev_id'=>'Jenisctev ID']
+            );
+    ?>
 
     <?= $form->field($model, 'no_reg')->textInput(['maxlength' => true]) ?>
 
